a83b747f7a8477a4c0b4d531e8684dbf01acf00e
AndrewLondon <andrewmart@mail.com>
2023-05-13 21:44:59 +0100

    Updated

--- top100liked/longest-common-prefix.js
+++ top100liked/longest-common-prefix.js
@@ -17,4 +17,29 @@ Related Topics
 String, Trie
 */
 
+var longestCommonPrefix = function (strs) {
+    // If the input array is empty or undefined, return an empty string
+    if (!strs || strs.length === 0) {
+        return "";
+    }
+
+    // Initialize prefix to be the first string in the array
+    let prefix = strs[0];
+
+    // Loop through all the strings in the array starting from the second string
+    for (let i = 1; i < strs.length; i++) {
+        // While the prefix is not a substring of the current string, remove the last character from the prefix
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, prefix.length - 1);
+            // If the prefix becomes empty, there is no common prefix, return an empty string
+            if (prefix === "") {
+                return "";
+            }
+        }
+    }
+
+    // Return the longest common prefix
+    return prefix;
+};
+
 
