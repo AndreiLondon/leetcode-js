8af5cbfefca7830f7010dc23eb422c58afe25a08
AndrewLondon <andrewmart@mail.com>
2023-05-22 20:08:19 +0100

    Updated

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -131,3 +131,128 @@ function ListNode(val) {
     return dummy.next; // Return the head of the merged linked list
   }
   
+
+// Other approach
+/*
+To solve this problem, we can use recursion, calling it until one of the sheets is null, 
+and while it is not null, we do an equality check, if list1 is less than or equal to list2, 
+we call recursion with list1.next = mergeTwoLists (list1.next, list2), 
+else if the value is greater than list2.next = mergeTwoLists(list1, list2.next). 
+If one of the sheets is null, we simply return the opposite list 
+(we don't care if it's null or not). Thus, when the recursion ends, the stack will begin 
+to collapse in reverse order, which will allow us to get a new merged sorted list.
+*/
+// Recursive approach
+var mergeTwoLists = function (l1, l2) {
+  if (!l1) return l2;
+  else if (!l2) return l1;
+  else if (l1.val <= l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+  } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2
+  }
+};
+
+/*
+The first if condition checks if l1 is null or empty. 
+If it is, it means there are no elements in the first list, 
+so the function simply returns l2, 
+which could be the second list or null itself if it's also empty.
+
+The second else if condition checks if l2 is null or empty. 
+If it is, it means there are no elements in the second list, 
+so the function simply returns l1, which could be the first 
+list or null itself if it's also empty.
+
+If neither of the lists is empty, 
+the code proceeds to the next else if condition. 
+This condition checks if the value of the first node in 
+l1 is less than or equal to the value of the first node in l2. 
+This comparison is based on the val property of the nodes.
+
+If the condition is true, it means that the first node in l1 
+should come before the first node in l2 in the merged list. 
+In this case, the code recursively calls the mergeTwoLists 
+function with l1.next (the next node in l1) as the first argument 
+and l2 as the second argument. The result of this recursive call is 
+assigned as the next node of l1, effectively merging the lists. 
+Finally, the function returns l1 as the head of the merged list.
+
+If the condition in step 3 is false, 
+it means that the first node in l2 should come before the first 
+node in l1 in the merged list. In this case, the code recursively calls 
+the mergeTwoLists function with l1 as the first argument and l2.next 
+(the next node in l2) as the second argument. 
+The result of this recursive call is assigned as the next node of l2, 
+effectively merging the lists. 
+Finally, the function returns l2 as the head of the merged list.
+
+By recursively applying this logic, 
+the function merges the two lists in a sorted order, 
+ensuring that the resulting linked list is sorted 
+based on the values of the nodes.
+*/
+
+//Iterative approach
+
+var mergeTwoLists = function(list1, list2) {
+  let ans = new ListNode();
+  let ptr = ans;
+  while(list1 && list2){
+      if(list1.val <= list2.val){
+          ans.next = new ListNode(list1.val);
+          list1 = list1.next;
+      }
+      else{
+          ans.next = new ListNode(list2.val);
+          list2 = list2.next;
+      }
+      ans = ans.next;
+  }
+  while(list1){
+      ans.next = new ListNode(list1.val);
+      list1 = list1.next;
+      ans = ans.next;
+  }
+  while(list2){
+      ans.next = new ListNode(list2.val);
+      list2 = list2.next;
+      ans = ans.next;
+  }
+  return ptr.next;
+};
+
+//Other approach
+
+var mergeTwoLists = function (list1, list2) {
+  let head = new ListNode, tail
+  if (!list1 || !list2)
+      return list2 || list1
+
+  if (list1.val < list2.val) {
+      head = list1
+      list1 = list1.next
+  }
+  else {
+      head = list2
+      list2 = list2.next
+  }
+  tail = head
+  while (list1 && list2) {
+      if (list1.val < list2.val) {
+          tail.next = list1
+          tail = tail.next
+          list1 = list1.next
+      } else {
+          tail.next = list2
+          tail = tail.next
+          list2 = list2.next
+      }
+  }
+   tail.next = list1 || list2
+  return head
+
+};
+1
