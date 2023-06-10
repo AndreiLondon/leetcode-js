284a2eb784eb61dd42812418dab84b2737ea2d26
AndrewLondon <andrewmart@mail.com>
2023-06-10 21:39:39 +0100

    Updated

--- explore/arraysandstrings/sliding-window.js
+++ explore/arraysandstrings/sliding-window.js
@@ -22,3 +22,25 @@ Find the longest subarray with a sum less than or equal to k
 Find the longest substring that has at most one "0"
 Find the number of subarrays that have a product less than k
 */
+
+//Implementation
+
+/*
+we need to identify a constraint metric. 
+In our example, the constraint metric is the sum of the window.
+*/
+
+function fn(nums, k) {
+    let left = 0;
+    let curr = 0;
+    let answer = 0;
+    for (let right = 0; right < nums.length; right++) {
+        curr += nums[right];
+        while (curr > k) {
+            curr -= nums[left];
+            left++;
+        }
+        answer = max(answer, right - left + 1);
+    }
+    return answer;
+}
