624aa305fb284a0c4f03f84547546882cfd15e54
AndrewLondon <andrewmart@mail.com>
2023-06-10 21:32:15 +0100

    Updated

---
+++ explore/arraysandstrings/sliding-window.js
@@ -0,0 +1,24 @@
+// Sliding Window
+
+/* contiguous - sharing a common border; touching.
+a subarray is a contiguous section of the array. 
+All the elements must be adjacent to each other in 
+the original array and in their original order.
+[1, 2, 3, 4]
+
+[1], [2], [3], [4]
+[1, 2], [2, 3], [3, 4]
+[1, 2, 3], [2, 3, 4]
+
+
+
+A subarray can be defined by two indices, the start and end. 
+
+Whenever a problem description talks about subarrays,
+you should figure out if sliding window is a good option
+by analyzing the problem description.
+
+Find the longest subarray with a sum less than or equal to k
+Find the longest substring that has at most one "0"
+Find the number of subarrays that have a product less than k
+*/
