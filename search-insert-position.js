c8d2d4409d06e46c2939b817c2a195549e46bf3a
AndrewLondon <andrewmart@mail.com>
2023-07-09 18:21:20 +0100

    Updated

--- search-insert-position.js
+++ search-insert-position.js
@@ -122,5 +122,99 @@ var searchInsert = function(nums, target) {
     return nums.indexOf(target); //returns index method//
    return [...nums, target].sort((a,b) => a-b).indexOf(target); //return where it shold be inserted//
 
-  
 };
+
+/*
+In JavaScript, the ellipsis or "dots" can be used in various contexts. 
+Here are a few use cases:
+
+1. Spread Syntax: The spread syntax, represented by three dots (...), 
+allows you to expand an iterable (like an array or string) 
+into individual elements. 
+It is commonly used in function calls or array literals.
+
+const numbers = [1, 2, 3];
+console.log(...numbers); // Output: 1 2 3
+
+const array1 = [1, 2, 3];
+const array2 = [...array1, 4, 5, 6];
+console.log(array2); // Output: [1, 2, 3, 4, 5, 6]
+
+2. Rest Parameters: The rest parameter syntax, also represented 
+by three dots (...), allows you to represent an indefinite number 
+of arguments as an array within a function. 
+It captures the remaining arguments into an array, 
+making it useful when you want to work with an 
+arbitrary number of parameters. 
+
+function sum(...numbers) {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sum(1, 2, 3)); // Output: 6
+console.log(sum(4, 5, 6, 7, 8)); // Output: 30
+
+3. Object Spread Syntax: Starting from ECMAScript 2018 (ES2018), 
+JavaScript also introduced the object spread syntax, 
+which allows you to create shallow copies of objects or 
+merge multiple objects into a new object. 
+It uses the same three-dot notation.
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+
+const mergedObj = { ...obj1, ...obj2 };
+console.log(mergedObj); // Output: { a: 1, b: 2, c: 3, d: 4 }
+
+function multiply(multiplier, ...numbers) {
+  return numbers.map(num => num * multiplier);
+}
+
+console.log(multiply(2, 1, 2, 3)); // Output: [2, 4, 6]
+console.log(multiply(3, 4, 5, 6)); // Output: [12, 15, 18]
+
+Arrow function expressions in JavaScript provide a concise syntax 
+for defining functions. They are commonly used when you need to 
+define anonymous functions or when you want to preserve the 
+lexical this binding. Here are some examples to 
+explain arrow function expressions:
+
+1. Basic Syntax:
+const square = (x) => {
+  return x * x;
+};
+console.log(square(5)); // Output: 25
+
+2. Implicit Return:
+const double = (x) => x * 2;
+console.log(double(3)); // Output: 6
+
+3. No Parameters:
+const sayHello = () => {
+  console.log("Hello!");
+};
+sayHello(); // Output: Hello!
+
+4. Lexical this Binding:
+Arrow functions have a lexical this binding, which means 
+that they inherit the this value from the surrounding code. 
+This behavior is different from regular functions, 
+where this is determined by how the function is called. 
+
+function Person() {
+  this.age = 0;
+
+  setInterval(() => {
+    this.age++;
+    console.log(this.age);
+  }, 1000);
+}
+
+const person = new Person();
+
+
+
+
+
+
+*/
